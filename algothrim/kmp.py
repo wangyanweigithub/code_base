0d1a0e1d1349d82a5f3ddbac0a6b54a48572565b
vwangyanwei <vwangyanwei@sohu.com>
2020-01-20 02:55:40 +0800

    up

--- algothrim/kmp.py
+++ algothrim/kmp.py
@@ -1,19 +1,27 @@
-def kmp(p, target):
+def kmp(p, target, pnext):
     i, j = 0, 0
     m, n = len(p), len(target)
-    for i < m and j < n:
+    while (i < m and j < n):
         if p[i] == target[j] or i == -1:
             i, j = i + 1, j + 1
         else:
-            j = pnext(j)
+            j = pnext[j]
     if i == len(p) - 1:
         return j - i
     
 
 def gen(p):
-    i, j, m = 0, 0, len(p) - 1
+    i, j, m = 0, -1, len(p)
     pnext = [-1] * m
-    for i < m - 1:
-        if p[i] == p[j]:
-            p[j+1]
+    while i < (m - 1):
+        if j==-1 or p[i] == p[j]:
+            i, j = i+1, j+1
+            pnext[i] = j
+        else:
+            j = pnext[j]
 
+    return pnext
+
+pnext = gen('aab')
+res = kmp('aab', 'bcaabc', pnext)
+print(res)
